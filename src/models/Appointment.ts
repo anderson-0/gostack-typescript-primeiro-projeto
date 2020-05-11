d440b143c796c4607ca16f0eb295a31528a6f94e
AndersonDaSilvaOSF <43174799+AndersonDaSilvaOSF@users.noreply.github.com>
2020-05-11 16:48:42 -0400

    primeiro commit
    Estrutura base com typeorm + postgres
    Create and List Appointments

---
+++ src/models/Appointment.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider: string;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  // By Using Entity the code below is automatically generated
+  // constructor({ date, provider }: Omit<Appointment, 'id'>) {
+  //   this.id = uuid();
+  //   this.provider = provider;
+  //   this.date = date;
+  // }
+}
+
+export default Appointment;
